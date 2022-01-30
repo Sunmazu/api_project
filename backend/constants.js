f099164c34620f40927603e8f67ce3a2c9eda28e
Sunmazu <sunm4zu@gmail.com>
2022-01-30 19:59:51 +0100

    MG - init commit

---
+++ backend/constants.js
@@ -0,0 +1,21 @@
+﻿const CONSTANTS = {};
+CONSTANTS.ENDPOINT = {};
+CONSTANTS.DATA = {};
+
+CONSTANTS.PORT = process.env.PORT || "3001";
+CONSTANTS.ENDPOINT.MASTERDETAIL = "/masterdetail";
+
+CONSTANTS.DATA.CATFACTS = [
+    'cat fact 1',
+    'cat fact 2',
+    'cat fact 3',
+    'cat fact 4',
+    'cat fact 5',
+];
+
+CONSTANTS.ENDPOINT.LIST = "/list";
+
+CONSTANTS.ENDPOINT.GRID = "/grid";
+
+
+module.exports = CONSTANTS;
---
+++ frontend/src/app/app-shell/list/list.component.ts
@@ -0,0 +1,53 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { ListService } from './list.service';
+import { IListItem } from './list.model';
+import { Subject, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  listItems$: Observable<IListItem[]> = new Observable();
+  private dataSource: Subject<IListItem[]> = new Subject();
+  warningMessageText = '';
+  warningMessageOpen = false;
+
+  constructor(private listService: ListService) {}
+
+  ngOnInit(): void {
+    this.listItems$ = this.dataSource.asObservable();
+    this.loadItems();
+  }
+
+  loadItems(): void {
+    this.listService.getListItems().subscribe(
+      (listItem: IListItem[]) => this.dataSource.next(listItem),
+      error => this.handleError(`Request to get list items failed: ${error}`)
+    );
+  }
+
+  addItem(inputText: string): void {
+    this.listService.addListItem(inputText).subscribe(
+      () => this.loadItems(), error => this.handleError(`Request to add item failed: ${error}`)
+    );
+  }
+
+  deleteItem(id: string): void {
+    this.listService.deleteListItem(id).subscribe(
+      () => this.loadItems(), error => this.handleError(`Request to delete item failed: ${error}`)
+    );
+  }
+
+  handleWarningClose(open: boolean): void {
+    this.warningMessageOpen = open;
+    this.warningMessageText = '';
+  }
+
+  private handleError(warningMessageText: string): void {
+    this.warningMessageOpen = true;
+    this.warningMessageText = warningMessageText;
+  }
+}
